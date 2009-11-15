54dcbd467ee7517eb7ab71a80958bf86fd1c59d8
Chih-Wei Huang <cwhuang@linux.org.tw>
2009-11-15 18:56:38 +0800

    KeyboardPicker: refine the keyboard layout list to be more extendable
    Also add Japanese layout to the list.

--- src/com/android/settings/KeyboardPicker.java
+++ src/com/android/settings/KeyboardPicker.java
@@ -2,18 +2,21 @@ package com.android.settings;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeMap;
 
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.os.SystemProperties;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class KeyboardPicker extends ListActivity{
-	int getContentView() {
+public class KeyboardPicker extends ListActivity {
+    private List<String> mKeyboardList;
+    private TreeMap<String,String> mKeyboardLayouts;
+
+    int getContentView() {
         return R.layout.keyboard_picker;
     }
 
@@ -23,46 +26,27 @@ public class KeyboardPicker extends ListActivity{
         setContentView(getContentView());
         int layoutId = R.layout.keyboard_picker_item;
         int fieldId = R.id.keyboard;
-        List<String> mKeyboardLayout = new ArrayList<String>();
-        mKeyboardLayout.add("American(Standard keyboard)");
-        mKeyboardLayout.add("Finnish");
-        mKeyboardLayout.add("French");
-        mKeyboardLayout.add("German");
-        mKeyboardLayout.add("Ireland");
-        mKeyboardLayout.add("Russia");
-        mKeyboardLayout.add("Spanish(Latin America)");
+        mKeyboardLayouts = new TreeMap<String,String>();
+        mKeyboardLayouts.put("American(Standard keyboard)", "qwerty");
+        mKeyboardLayouts.put("Finnish", "fn");
+        mKeyboardLayouts.put("French", "fr");
+        mKeyboardLayouts.put("German", "de");
+        mKeyboardLayouts.put("Ireland", "Ireland");
+        mKeyboardLayouts.put("Japanese", "jp");
+        mKeyboardLayouts.put("Russia", "ru");
+        mKeyboardLayouts.put("Spanish(Latin America)", "spanish_latin");
 
-
+        mKeyboardList = new ArrayList<String>(mKeyboardLayouts.keySet());
         ArrayAdapter<String> adapter =
-            new ArrayAdapter<String>(this, layoutId, fieldId, mKeyboardLayout);
+                new ArrayAdapter<String>(this, layoutId, fieldId, mKeyboardList);
         getListView().setAdapter(adapter);
     }
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
-        switch(position){
-		case 1:
-			SystemProperties.set("persist.sys.keylayout","fn");
-			break;
-		case 2:
-			SystemProperties.set("persist.sys.keylayout","fr");
-			break;
-		case 3:
-			SystemProperties.set("persist.sys.keylayout","de");
-			break;
-		case 4:
-			SystemProperties.set("persist.sys.keylayout","Ireland");
-			break;
-		case 5:
-			SystemProperties.set("persist.sys.keylayout","ru");
-			break;
-		case 6:
-			SystemProperties.set("persist.sys.keylayout","spanish_latin");
-			break;
-		default:
-			SystemProperties.set("persist.sys.keylayout","qwerty");
-			break;
-	  }
-
-        Toast.makeText(this, "Please reboot your machine to enable the new keyboard layout.", Toast.LENGTH_LONG).show();
+        String kb = mKeyboardList.get(position);
+        String name = mKeyboardLayouts.get(kb);
+        SystemProperties.set("persist.sys.keylayout", name);
+        Toast.makeText(this, "Set keyboard layout to " + kb +
+                ".\nPlease reboot your machine to enable the new keyboard layout.", Toast.LENGTH_LONG).show();
     }
 }
