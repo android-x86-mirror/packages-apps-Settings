a62f00e811a599e208671f8cd26b89c1d6258e1c
Jaap Jan Meijer <jjmeijer88@gmail.com>
2017-02-25 12:01:10 +0100

    Revert "Settings: Add Superuser entry"
    This reverts commit e99f887cdd1920c71869614820f112172d40270f.

--- Android.mk
+++ Android.mk
@@ -25,17 +25,13 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := \
-    $(call all-java-files-under, src) \
-    $(call all-java-files-under,../../../external/koush/Widgets/Widgets/src) \
-    $(call all-java-files-under,../../../external/koush/Superuser/Superuser/src) \
+        $(call all-java-files-under, src)
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     frameworks/support/v7/preference/res \
     frameworks/support/v14/preference/res \
     frameworks/support/v7/appcompat/res \
-    frameworks/support/v7/recyclerview/res \
-    external/koush/Widgets/Widgets/res \
-    external/koush/Superuser/Superuser/res \
+    frameworks/support/v7/recyclerview/res
 
 LOCAL_PACKAGE_NAME := Settings
 LOCAL_CERTIFICATE := platform
@@ -43,10 +39,8 @@ LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
-LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
-
 LOCAL_AAPT_FLAGS := --auto-add-overlay \
-    --extra-packages android.support.v7.preference:android.support.v14.preference:android.support.v17.preference:android.support.v7.appcompat:android.support.v7.recyclerview:com.koushikdutta.superuser:com.koushikdutta.widgets
+    --extra-packages android.support.v7.preference:android.support.v14.preference:android.support.v17.preference:android.support.v7.appcompat:android.support.v7.recyclerview
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
